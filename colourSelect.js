1c89712961f535ebaff2de1940d854f1c528635e
GIVEMEFOOD <danielallcorn5@gmail.com>
2024-04-30 10:42:13 +0100

    added javascript to try make hex work it dosnt yet tho

--- colourSelect.js
+++ colourSelect.js
@@ -1,8 +1,18 @@
-function getHexColor() {
-    var letters ='0123456789ABCDEF';
-    var color = '#';
-    for (var i =0; i < 6; i++) {
-        color += letters[Math.floor(math.random() * 16)];
-    }
-    return color;
+function updateColor() {
+    var red = document.getElementById("R").value;
+    var green = document.getElementById("G").value;
+    var blue = document.getElementById("B").value;
+
+    var hexColour = "#" + red + green + blue;
+
+    var colorBox = document.getElementById("color-box");
+    colorBox.style.backgroundColor = hexColour;
 }
+
+var inputs = document.querySelectorAll('input[type="text"]');
+inputs.forEach(function(input) {
+    input.addEventListener("input", updateColor);
+});
+
+// Initial call to set initial color
+updateColor();
