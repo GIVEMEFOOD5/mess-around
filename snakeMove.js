dfe00b771e6ca455238f9657c4c624b60a0cd991
GIVEMEFOOD <danielallcorn5@gmail.com>
2024-05-13 15:19:10 +0100

    added some files

--- snakeMove.js
+++ snakeMove.js
@@ -1,43 +1,28 @@
-document.addEventListener("keydown", (e) => {
-    const image = document.createElement("img");
-    image.src = "unicorn1.png";
-    image.style.position = "absolute";
+let inputDirection = { x: 0, y: 0 }
+let lastInputDirection = { x: 0, y: 0 }
 
-    let left = parseInt(image.style.left) || 0;
-    let top = parseInt(image.style.top) || 0;
+window.addEventListener('keydown', e => {
+  switch (e.key) {
+    case 'ArrowUp':
+      if (lastInputDirection.y !== 0) break
+      inputDirection = { x: 0, y: -1 }
+      break
+    case 'ArrowDown':
+      if (lastInputDirection.y !== 0) break
+      inputDirection = { x: 0, y: 1 }
+      break
+    case 'ArrowLeft':
+      if (lastInputDirection.x !== 0) break
+      inputDirection = { x: -1, y: 0 }
+      break
+    case 'ArrowRight':
+      if (lastInputDirection.x !== 0) break
+      inputDirection = { x: 1, y: 0 }
+      break
+  }
+})
 
-    const moveSpeed = 10; // Adjust this value to change the speed of movement
-
-    switch (e.key) {
-        case "ArrowLeft":
-            left -= moveSpeed;
-            break;
-        case "ArrowRight":
-            left += moveSpeed;
-            break;
-        case "ArrowUp":
-            top -= moveSpeed;
-            break;
-        case "ArrowDown":
-            top += moveSpeed;
-            break;
-        default:
-            return; // Do nothing if other keys are pressed
-    } 
-
-    image.style.left = `${left}px`;
-    image.style.top = `${top}px`;
-
-    document.body.appendChild(image);
-
-    image.style.transition = "0.25s ease";
-
-    setTimeout(() => {
-        image.style.opacity = "0";
-        image.style.filter = "blur(5px)";
-    }, 500);
-
-    setTimeout(() => {
-        image.remove();
-    }, 750);
-});
+export function getInputDirection() {
+  lastInputDirection = inputDirection
+  return inputDirection
+}
